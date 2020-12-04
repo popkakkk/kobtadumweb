b9f5aa6a1f6db5fbc5e0e53957cbb78beadb71d9
poppatipan39 <sir.poppatipan@gmail.com>
2020-12-04 14:42:39 +0700

    Navbar Change

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function Navbar() {
-  return <header className="Navbar">Facebook</header>;
+  return <header className="Navbar">Pop Patipan Chart</header>;
 }
 
 export default Navbar;
